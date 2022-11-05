7d1dcea3c58511e047f99c04d171dcbc973e88fb
Daniel Huber <30466471+daniel0611@users.noreply.github.com>
2022-11-05 18:18:47 +0100

    Add confirmation dialog for deletions

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AddComponent } from './add/add.component';
-import { ListComponent } from './list/list.component';
+import { ConfirmDeletionDialog, ListComponent } from './list/list.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
@@ -12,9 +12,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [AppComponent, AddComponent, ListComponent],
+  declarations: [AppComponent, AddComponent, ListComponent, ConfirmDeletionDialog],
   imports: [
     BrowserModule,
     HttpClientModule,
@@ -26,6 +27,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatIconModule,
     MatInputModule,
     MatCheckboxModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- frontend/src/app/list/list.component.ts
+++ frontend/src/app/list/list.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { TodoItem } from '../todo';
 import { TodoService } from '../todo.service';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
   todoItems: TodoItem[] = [];
 
-  constructor(private todoService: TodoService) { }
+  constructor(private todoService: TodoService, private dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.todoService.getTodoItems().subscribe((data) => {
@@ -19,7 +24,16 @@ export class ListComponent implements OnInit {
   }
 
   deleteItem(item: TodoItem): void {
-    this.todoService.deleteTodoItem(item);
+    const dialogRef = this.dialog.open(ConfirmDeletionDialog, {
+      width: '600px',
+      data: item,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.todoService.deleteTodoItem(item);
+      }
+    });
   }
 
   updateTodo(item: TodoItem): void {
@@ -27,3 +41,21 @@ export class ListComponent implements OnInit {
   }
 }
 
+@Component({
+  selector: 'app-confirm-deletion-dialog',
+  templateUrl: 'confirm-deletion-dialog.html',
+})
+export class ConfirmDeletionDialog {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmDeletionDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: TodoItem
+  ) {}
+
+  confirm(): void {
+    this.dialogRef.close(true);
+  }
+
+  cancel(): void {
+    this.dialogRef.close(false);
+  }
+}
